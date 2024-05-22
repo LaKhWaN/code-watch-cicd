6fc50e3cf1fe93a8f45198de8b9491510eb08f24
Upender Singh Lakhwan <lakhwanus009@gmail.com>
2024-05-22 23:22:12 +0530

    added file

---
+++ sum.py
@@ -0,0 +1,17 @@
+# sum.py
+
+import sys
+
+def sum_two_numbers(num1, num2):
+    return num1 + num2
+
+if __name__ == "__main__":
+    # Read input
+    input_data = sys.stdin.read()
+    num1, num2 = map(float, input_data.strip().split())
+    
+    # Calculate sum
+    result = sum_two_numbers(num1, num2)
+    
+    # Print result
+    print(result)
